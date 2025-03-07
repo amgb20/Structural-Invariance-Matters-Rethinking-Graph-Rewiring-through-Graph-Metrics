b53b4770e1a2e468b24aed34087454f54287450e
Catherine Aitken <catherine.p.aitken@gmail.com>
2025-03-07 14:29:45 +0000

    GTR stuff

--- gtr_rewire.py
+++ gtr_rewire.py
@@ -8,16 +8,40 @@ datasets = ["REDDIT-BINARY", "IMDB-BINARY", "MUTAG", "ENZYMES", "PROTEINS", "COL
 
 #data_dict = {name: TUDataset(root=f'./tmp/'+name, name=name) for name in datasets}
 
-def rewire_gtr(name, num_edges, add_edges):
-    pre_transform = T.Compose([PrecomputeGTREdges(num_edges=num_edges)])
-    transform = T.Compose([AddPrecomputedGTREdges(num_edges=add_edges)])
+def rewire_gtr(name, data_w_edge, num_edges, add_edges):
+    #pre_transform = T.Compose([PrecomputeGTREdges(num_edges=num_edges)])
+    #transform = T.Compose([AddPrecomputedGTREdges(num_edges=add_edges)])
+    pre_transform = PrecomputeGTREdges(num_edges=num_edges)
+    transform = AddPrecomputedGTREdges(num_edges=add_edges)
 
-    dataset = TUDataset(
-        root="./tmp/",
-        name=name,
-        transform=transform,
-        pre_transform=pre_transform
-    )
+    #dataset = TUDataset(
+    #    root="./tmp/",
+    #    name=name,
+    #    transform=transform,
+    #    pre_transform=pre_transform
+    #)
+    #Adding precomputed edges if it doesn't have any
+    #for data in dataset:
+    #    if not hasattr(data, "precomputed_gtr_edges"):
+    #        data.precomputed_gtr_edges = PrecomputeGTREdges(num_edges=30)(data).precomputed_gtr_edges
+
+    #Trying just having the dataset sent in
+    for i in range(len(data_w_edge)):
+        if not hasattr(data_w_edge[i], "precomputed_gtr_edges"):
+            #print(f"⚠️ Adding precomputed GTR edges for dataset: {name}")
+            data_w_edge[i].precomputed_gtr_edges = pre_transform(data_w_edge[i]).precomputed_gtr_edges
+            data_w_edge[i]["precomputed_gtr_edges"] = data_w_edge[i].precomputed_gtr_edges
+
+
+    #print(type(data_w_edge))
+
+    dataset = data_w_edge
+    for data in data_w_edge:
+        if not hasattr(data, "precomputed_gtr_edges"):
+            print("Edges got lost")
+
+    for data in dataset:
+        data = transform(data)
 
     if all([
     hasattr(data, "precomputed_gtr_edges") and data.precomputed_gtr_edges.shape[1] == (2*num_edges)
@@ -40,5 +64,3 @@ def rewire_gtr(name, num_edges, add_edges):
 
     return dataset
 
-
-rewire_gtr(name='MUTAG', num_edges=30, add_edges=20)
